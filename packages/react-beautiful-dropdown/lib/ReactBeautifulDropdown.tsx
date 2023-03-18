288360f0bc38e6110537ed30f9856665c4ea1097
daxter-army <mehulsaini456@gmail.com>
2023-03-18 14:24:00 +0530

    bumped version of react-beautiful-dropdown to 0.0.7

--- packages/react-beautiful-dropdown/lib/ReactBeautifulDropdown.tsx
+++ packages/react-beautiful-dropdown/lib/ReactBeautifulDropdown.tsx
@@ -11,8 +11,8 @@ import S from './Styles'
 import InputProps from './Props'
 
 const Input: React.FC<InputProps> = ({
-	value = "Sample value",
-	error = { isError: false, errorText: "" },
+	value,
+	error,
 	options,
 	inputRef,
 	isActive,
@@ -20,7 +20,7 @@ const Input: React.FC<InputProps> = ({
 	optionsRef,
 	focusIndex,
 	activeIndex,
-	valueHandler = (val) => { console.log("Value:", val, "\n", "Pass custom valueHandler() to get latest value back to you") },
+	valueHandler,
 	optionItemsRef,
 	isActiveHandler,
 	onKeyDownHandler,
@@ -33,12 +33,12 @@ const Input: React.FC<InputProps> = ({
 	isMouseHoverAllowedHandler,
 	placeholder = "Placeholder",
 }) => {
-	// TO DETECT OUTSIDE CLICK
+	// To detect outisde clicks
 	const dropdownRef = useRef([inputRef, optionsRef])
 	useOutsideClick(dropdownRef, () => {
 		if (isActive) {
-			if (activeIndex !== focusIndex) valueHandler(prevValue)
-			isActiveHandler(false)
+			// To only change when the value is changed
+			if ((activeIndex !== focusIndex) || (activeIndex === -1 && focusIndex === -1)) valueHandler(prevValue)
 			isActiveHandler(false)
 			inputRef.current?.blur()
 		}
@@ -48,13 +48,10 @@ const Input: React.FC<InputProps> = ({
 		isActiveHandler(true)
 	}
 
-	const blurHandler = () => {
-		// setIsActive(false)
-	}
-
-	const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-		valueHandler(e.target.value)
-	}
+	// const blurHandler = () => {
+	// Because I don't want to unmount on input blur
+	// 	setIsActive(false)
+	// }
 
 	return (
 		<S.InputWpr>
@@ -63,25 +60,28 @@ const Input: React.FC<InputProps> = ({
 					value={value}
 					ref={inputRef}
 					isActive={isActive}
-					onBlur={blurHandler}
+					// onBlur={blurHandler}
 					onFocus={focusHandler}
 					placeholder={placeholder}
 					style={customInputStyles}
 					onKeyDown={onKeyDownHandler}
-					onChange={onChangeHandler}
+					onChange={e => valueHandler(e.target.value)}
 				/>
 				<S.IconCtr>
-					{isActive
-						? <FiChevronUp size={18} color={isActive ? '#3498db' : "#777"} />
-						: <FiChevronDown size={18} color={isActive ? '#3498db' : "#777"} />
+					{
+						isActive
+							? <FiChevronUp size={18} color={isActive ? '#3498db' : "#777"} />
+							: <FiChevronDown size={18} color={isActive ? '#3498db' : "#777"} />
 					}
 				</S.IconCtr>
 			</S.InputCtr>
-			{error.isError && <S.ErrorLabel>{error.errorText}<MdErrorOutline /></S.ErrorLabel>}
+			{
+				error.isError && <S.ErrorLabel>{error.errorText}<MdErrorOutline /></S.ErrorLabel>
+			}
 			{
 				isActive && <Dropdown
 					list={options}
-					optionsRef={optionsRef}
+					ref={optionsRef}
 					focusIndex={focusIndex}
 					activeIndex={activeIndex}
 					optionItemsRef={optionItemsRef}
